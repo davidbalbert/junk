25a99cec2b805c4cd5c93c3f6fbe82cf120c0a8f
David Albert <davidbalbert@gmail.com>
2011-07-19 18:04:07 -0400

    implemented "add" command and gitignore support

--- lib/junk/command.rb
+++ lib/junk/command.rb
@@ -1,5 +1,6 @@
 require 'thor'
 require 'fileutils'
+require 'pathname'
 
 module Junk
   class Command < Thor
@@ -30,6 +31,8 @@ module Junk
       check_for_git!
       setup unless prefix_is_setup?
 
+      drawer_name = get_drawer_name(Dir.pwd)
+
       inside(junk_home) do
         if File.exists?(File.join(junk_home, drawer_name))
           error "There is already a junk drawer called #{drawer_name}."
@@ -37,17 +40,53 @@ module Junk
         end
 
         Dir.mkdir drawer_name
-        say "Alright, #{Dir.pwd} now has a junk drawer."
       end
+
+      File.symlink(File.join(junk_home, drawer_name), File.join(Dir.pwd, ".junk"))
+
+      add_to_git_ignore(".junk")
+
+      say "Alright, #{Dir.pwd} now has a junk drawer."
     end
 
     desc "add FILE", "Moves FILE to your junk drawer and symlinks it from it's old location"
     def add(file)
-      # TODO: Make this do something
-      # deal with recursive directory creation if the path is deep
-      # instead of recursively adding, could do gross filename substitution and keep it flat in the junk drawer
-      #
-      # TODO: also add appending to .gitignore if it exists
+      inside(find_junk_drawer_symlink!) do
+        unless File.exists? file
+          error "#{file} doesn't seem to exist."
+          exit(1)
+        end
+
+        if file[0] == '/'
+          pwd = Pathname.new(Dir.pwd)
+          file_path = Pathname.new(file)
+          relative_path = file_path.relative_path_from(pwd).to_s
+        else
+          relative_path = file
+        end
+
+        if File.directory? relative_path
+          error "junk doesn't support adding directories, only single files :(."
+          exit(1)
+        end
+
+        dir, filename = File.split(relative_path)
+        new_path = File.join(junk_drawer_for_directory(Dir.pwd), dir)
+        unless File.exists? new_path
+          FileUtils.mkpath(new_path)
+        end
+
+        new_path = File.join(junk_drawer_for_directory(Dir.pwd), relative_path)
+        if File.exists? new_path
+          error "it looks like you've already added #{file}"
+          exit(1)
+        end
+
+        FileUtils.mv(relative_path, new_path)
+        File.symlink(new_path, relative_path)
+
+        add_to_git_ignore(relative_path)
+      end
     end
 
     private
@@ -83,12 +122,39 @@ module Junk
       say
     end
 
-    def junk_home
-      @junk_home ||= File.join(ENV["HOME"], ".junk")
+    def find_junk_drawer_symlink!
+      old_pwd = Dir.pwd
+
+      loop do
+        if Dir.pwd == "/"
+          Dir.chdir old_pwd
+          error "it looks like this folder doesn't have a junk drawer"
+          exit(1)
+        elsif Dir.exists? ".junk"
+          junk_dir = Dir.pwd
+          Dir.chdir old_pwd
+          return junk_dir
+        end
+        Dir.chdir ".."
+      end
     end
 
-    def drawer_name
-      @drawer_name ||= File.basename(Dir.pwd)
+    def add_to_git_ignore(path)
+      if File.exists? ".gitignore"
+        File.open(".gitignore", "a") { |f| f.puts(path) }
+      end
+    end
+
+    def junk_home
+      @junk_home ||= File.join(ENV["HOME"], ".junkd")
+    end
+
+    def get_drawer_name(dir)
+      File.basename(dir)
+    end
+
+    def junk_drawer_for_directory(dir)
+      File.join(junk_home, File.basename(dir))
     end
 
     def error(str)
